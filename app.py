9792471483d4b431ae6cb7e166d7526119cb8e68
Batpurev <bbayrbaysgalan0203@email.com>
2020-01-26 12:43:14 +0800

    version1

---
+++ app.py
@@ -0,0 +1,16 @@
+#variable
+#data types int, float, str
+#input(), print(), type()
+# a=20
+# b=4
+# print(a//b)
+# a=int(input("a number:"))
+# b=int(input("b number:"))
+# print(a+b)
+# len() shows the lenght of str
+# upper() makes the letters BIG
+# lower() makes the letters smol
+# capitalize() captitalize the first letter
+# replace() replace certain letters 
+# FirstName="Tom"
+# print(FirstName.find("enter search"))
