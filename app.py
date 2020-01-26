ea60fcced9375a640e7e0bc9488de33a0e1f58a6
Bayarbayasgalan <60308166+Trollernator@users.noreply.github.com>
2020-01-26 13:13:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
 #variable
 #data types int, float, str
 #input(), print(), type()
-# a=20
-# b=4
-# print(a//b)
-# a=int(input("a number:"))
-# b=int(input("b number:"))
-# print(a+b)
 # len() shows the lenght of str
 # upper() makes the letters BIG
 # lower() makes the letters smol
